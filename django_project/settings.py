5508f41910cb59b5f529404f0233b9c7af6dba49
Rohit Singh <91629884+rohit80025@users.noreply.github.com>
2023-02-04 19:14:50 +0000

    add login page

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
